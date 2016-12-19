55e07322751aacf1ede80c328b343d12432cb99f
caitlin ashtari and haley swain <haley@>
2016-12-19 10:26:52 -0800

    add join table for playlists and lessons.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219173631) do
+ActiveRecord::Schema.define(version: 20161219181651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20161219173631) do
     t.string  "external_link"
     t.string  "tag"
     t.boolean "private"
+  end
+
+  create_table "lessons_playlists", force: :cascade do |t|
+    t.integer "lesson_id"
+    t.integer "playlist_id"
   end
 
   create_table "playlists", force: :cascade do |t|
