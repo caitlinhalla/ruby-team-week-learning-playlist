2435b3e5c5417c61e1b7afa3454b74f3bf17e276
Kenneth Black and Margie Nevarez <kenneth@>
2016-12-19 16:54:46 -0800

    end of day one, user auth nearly done

--- app.rb
+++ app.rb
@@ -8,6 +8,9 @@ require('./lib/playlist')
 require('./lib/student')
 require('./lib/lesson')
 require('warden')
+
+enable :sessions
+set :session_secret, "supersecret"
 require('./helpers/session')
 
 
@@ -22,4 +25,39 @@ end
 
 get('/dashboard') do
   erb(:student_dashboard)
+end
+
+get('/auth/login') do
+  erb(:login)
+end
+
+post('/auth/login') do
+  env['warden'].authenticate!
+
+  if session[:return_to].nil?
+    redirect '/'
+  else
+    redirect session[:return_to]
+  end
+end
+
+get '/auth/logout' do
+   env['warden'].raw_session.inspect
+   env['warden'].logout
+  #  flash[:success] = 'Successfully logged out'
+   redirect '/'
+ end
+
+post('/auth/unauthenticated') do
+  p "----------------ROUTE ACCESS--------------------"
+  session[:return_to] = env['warden.options'][:message] || "You must log in"
+
+  redirect '/auth/login'
+end
+
+
+get '/protected' do
+  env['warden'].authenticate!
+
+  erb :protected
 end
--- helpers/session.rb
+++ helpers/session.rb
@@ -1,40 +1,47 @@
-class App < Sinatra::Base
-  enable :sessions
-  # register Sinatra::Flash
+class Auth < Sinatra::Base
+  post('/auth/unauthenticated') do
+    p "----------------ROUTE ACCESS--------------------"
+    session[:return_to] = env['warden.options'][:message] || "You must log in"
 
-  use Warden::Manager do |config|
-    config.serialize_into_session{ |user| user.id }
-    config.serialize_from_session{ |id| User.get(id)}
+    redirect '/auth/login'
+  end
+end
 
-    config.scope_defaults :default,
-    strategies: [:password],
-    action: 'auth/unauthenticated'
-    config.failure_app = self
+
+
+use Warden::Manager do |config|
+  config.serialize_into_session{ |user| user.id }
+  config.serialize_from_session{ |id| User.get(id)}
+
+  config.scope_defaults :default,
+  strategies: [:password],
+  action: 'auth/unauthenticated'
+  config.failure_app = Auth
+end
+
+Warden::Manager.before_failure do |env, opts|
+  env['REQUEST_METHOD'] = 'POST'
+
+  env.each do |key, value|
+    env[key]['_method'] = 'post' if key == 'rack.request.form_hash'
+  end
+end
+
+Warden::Strategies.add(:password) do
+  def valid?
+    params['user'] && params['user']['username'] && params['user']['password']
   end
 
-  Warden::Manager.before_failure do |env, opts|
-    env['REQUEST_METHOD'] = 'POST'
+  def authenticate!
+    p params['user']['username']
+    user = User.where(:username => params['user']['username']).first
 
-    env.each do |key, value|
-      en[key]['_method'] = 'post' if key == 'rack.request.form_hash'
-    end
-  end
-
-  Warden::Strategies.add(:password) do
-    def valid?
-      params['user'] && params['user']['username'] && params['user']['password']
-    end
-
-    def authenticate!
-      user = User.first(:username => params['user']['username'])
-
-      if user.nil?
-        throw(:warden, :message => "The username you entered does not exist.")
-      elsif user.authenticate(params['user']['password'])
-        success!(user)
-      else
-        throw(:warden, :message => "The username and password combination")
-      end
+    if user.nil?
+      throw(:warden, :message => "The username you entered does not exist.")
+    elsif user.authenticate(params['user']['password'])
+      success!(user)
+    else
+      throw(:warden, :message => "The username and password combination")
     end
   end
 end
